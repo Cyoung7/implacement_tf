e5e50d3f5404592fce75e9cf040de44490300311
Cyoung7 <1594012301@qq.com>
2018-08-16 14:20:34 +0800

    remove test rename

--- ctc/train_main.py
+++ ctc/train_main.py
@@ -41,4 +41,4 @@ if __name__ == '__main__':
         fw_train.add_summary(summary, step)
         print('step:', step, ' ', 'train loss:', loss)
         # print('test git')
-        print('test rename')
+        # print('test rename')
